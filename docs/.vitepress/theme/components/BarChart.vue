1ae62de3da0838ed72873f83a41cb55abd67efc8
Peter Marcus Hoveling <pm.hoveling@pmhoveling.local>
2025-06-11 13:47:36 +0200

    test

--- docs/.vitepress/theme/components/BarChart.vue
+++ docs/.vitepress/theme/components/BarChart.vue
@@ -43,7 +43,7 @@ export default {
     }
   },
   mounted() {
-    fetch('/data/latest.csv')
+    fetch('/hkh-trading/data/latest.csv')
       .then((response) => response.text())
       .then((csv) => {
         const parsed = Papa.parse(csv, { header: true, skipEmptyLines: true })
